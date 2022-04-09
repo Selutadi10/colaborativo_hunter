c82ff171f3e01711a73503cbff8de9ec76ab6c86
inoricode28 <inori237@gmail.com>
2022-04-09 02:06:56 -0500

    cpp

---
+++ app.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+//Area global
+//variables globales
+string nombre="beteta", name;
+int main(){
+    //Area local
+    //Variables locales
+    //Realizare iteraciones
+    for (int i = 1; i <11; i++)
+    {
+        cout<<i<<" "<<nombre<<" es un hunters\n";
+    }
+    
+    
+    return 0;
+}
